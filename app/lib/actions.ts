a1fc3876fd64c1c06860fedee7aa4c40bc0d0f75
CanioV <2756432588@qq.com>
2025-03-30 21:24:34 +0800

    auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@
 import { z } from "zod"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
+
 import postgres from "postgres"
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" })
@@ -109,4 +112,20 @@ export async function deleteInvoice(id: string) {
     console.error(error)
   }
   revalidatePath("/dashboard/invoices")
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn("credentials", formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials."
+        default:
+          return "Something went wrong."
+      }
+    }
+    throw error
+  }
 }
