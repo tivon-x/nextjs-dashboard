3b958bb1fb1081338a9090fd30ba7a0767303a4f
CanioV <2756432588@qq.com>
2025-03-30 21:29:14 +0800

    metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Customers",
+  description: "Customers page",
+}
+
 export default function Page() {
   return <p>Customers Page</p>
 }
